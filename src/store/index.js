d25aff4ac783bbf7d09c7d4e10af59833b9535b3
Tiago Alves <tiagofarma39@gmail.com>
2022-02-15 14:41:59 -0400

    remove devtools

--- src/store/index.js
+++ src/store/index.js
@@ -5,25 +5,10 @@ import rootReducer from "./modules/rootReducer";
 import createSagaMiddleware from "redux-saga";
 import { composeWithDevTools } from "redux-devtools-extension";
 
+const sagaMiddleware = createSagaMiddleware();
 
-const sagaMiddleware = createSagaMiddleware()
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 
+sagaMiddleware.run(rootSaga);
 
-
-
-
-
-
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__
-    && window.__REDUX_DEVTOOLS_EXTENSION__()
-    ?
-    composeWithDevTools(applyMiddleware(sagaMiddleware))
-    :
-    applyMiddleware(sagaMiddleware)
-)
-
-sagaMiddleware.run(rootSaga)
-
-export default store
-
-
+export default store;
