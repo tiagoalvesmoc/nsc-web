f924afc776eea126a1295b99b823f32c1f99d2a7
Tiago Alves <tiagofarma39@gmail.com>
2022-02-15 14:53:16 -0400

    remove devtools

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,6 @@ import rootSaga from "./modules/rootSagas";
 import rootReducer from "./modules/rootReducer";
 
 import createSagaMiddleware from "redux-saga";
-import { composeWithDevTools } from "redux-devtools-extension";
 
 const sagaMiddleware = createSagaMiddleware();
 
